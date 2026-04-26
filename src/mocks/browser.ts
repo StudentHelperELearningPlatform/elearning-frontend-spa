fef1ca3ab28b0ed2b69c1e3ac8a20c788c8d5028
Stefan Stoean <119006813+NightEnergy@users.noreply.github.com>
2026-04-26 15:32:01 +0300

    Revert "Spring 3 quiz"

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,5 +1,4 @@
 import { setupWorker } from 'msw/browser';
 import { authHandlers } from './handlers/auth.handlers';
-import { quizzesHandlers } from './handlers/quizzes.handlers';
 
-export const worker = setupWorker(...authHandlers, ...quizzesHandlers);
+export const worker = setupWorker(...authHandlers);
