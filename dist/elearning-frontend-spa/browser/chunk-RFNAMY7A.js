cd2bc6245996b06c333670c0003b75d179da7815
ApetreiStefan <109090443+ApetreiStefan@users.noreply.github.com>
2026-05-21 16:24:14 +0300

    dubla 4
    asd

---
+++ dist/elearning-frontend-spa/browser/chunk-RFNAMY7A.js
@@ -0,0 +1,1 @@
+var e={production:!0,quizApiUrl:"https://e-learning-backend-team1-api-gateway.onrender.com",lessonApiUrl:"https://e-learning-backend-team1-api-gateway.onrender.com",userPlatformApiUrl:"https://e-learning-backend-team1-api-gateway.onrender.com",keycloak:{url:"https://keycloak-26-0-7-vb34.onrender.com/",realm:"elearning",clientId:"elearning-angular"}};export{e as a};
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,13 +1,14 @@
 // src/environments/environment.production.ts
 export const environment = {
   production: true,
-  quizApiUrl: 'https://content-api.elearning.com',
-  lessonApiUrl: 'https://content-api.elearning.com',
-  userPlatformApiUrl: 'https://platform-api.elearning.com',
+  quizApiUrl: 'https://e-learning-backend-team1-api-gateway.onrender.com',
+  lessonApiUrl: 'https://e-learning-backend-team1-api-gateway.onrender.com',
+  userPlatformApiUrl: 'https://e-learning-backend-team1-api-gateway.onrender.com',
 
   keycloak: {
-    url: 'https://auth.elearning.com',
+    url: 'https://keycloak-26-0-7-vb34.onrender.com/',
     realm: 'elearning',
-    clientId: 'elearning-spa'
+    clientId: 'elearning-angular'
   }
 };
+
