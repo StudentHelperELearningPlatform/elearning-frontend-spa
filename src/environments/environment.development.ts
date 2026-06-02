c4bd702abeafcf15892796173f2d224c8937f228
ariana834 <ariana.bercea260@yahoo.com>
2026-06-02 22:18:28 +0300

    fix: update API gateway URL to new Render deployment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: false,
   quizApiUrl: 'https://e-learning-backend-team1-api-gateway.onrender.com',
   lessonApiUrl: 'https://e-learning-backend-team1-api-gateway.onrender.com',
-  userPlatformApiUrl: 'https://e-learning-backend-team1-api-gateway.onrender.com',
+  userPlatformApiUrl: 'https://e-learning-backend-team1-api-gateway-s9dn.onrender.com',
   keycloak: {
     url: 'https://keycloak-26-0-7-vb34.onrender.com',
     realm: 'elearning',
