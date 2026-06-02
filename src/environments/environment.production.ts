3585a2d525576891a36be6919b28fa634bad08b5
Denisa-Ariana Bercea <ariana.bercea260@yahoo.com>
2026-06-02 22:22:19 +0300

    Fix/update gateway url (#180)
    * add vercel config
    
    * fix: update API gateway URL to new Render deployment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: true,
   quizApiUrl: 'https://e-learning-backend-team1-api-gateway.onrender.com',
   lessonApiUrl: 'https://e-learning-backend-team1-api-gateway.onrender.com',
-  userPlatformApiUrl: 'https://e-learning-backend-team1-api-gateway.onrender.com',
+  userPlatformApiUrl: 'https://e-learning-backend-team1-api-gateway-s9dn.onrender.com',
 
   keycloak: {
     url: 'https://keycloak-26-0-7-vb34.onrender.com',
