6c5ea5a9dae8b7b9c67adcccdcaa35c7598f5f9b
ariana834 <ariana.bercea260@yahoo.com>
2026-06-03 10:45:33 +0300

    change env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,13 +2,8 @@
 // NOT FINAL
 export const environment = {
   production: false,
-<<<<<<< HEAD
   quizApiUrl: ' https://e-learning-backend-team1-api-gateway-s9dn.onrender.com/',
   lessonApiUrl: ' https://e-learning-backend-team1-api-gateway-s9dn.onrender.com/',
-=======
-  quizApiUrl: 'https://e-learning-backend-team1-api-gateway.onrender.com',
-  lessonApiUrl: 'https://e-learning-backend-team1-api-gateway.onrender.com',
->>>>>>> 3585a2d525576891a36be6919b28fa634bad08b5
   userPlatformApiUrl: 'https://e-learning-backend-team1-api-gateway-s9dn.onrender.com',
   keycloak: {
     url: 'https://keycloak-26-0-7-vb34.onrender.com',
